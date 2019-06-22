43edb1d241b9542f5bf4eb684ae7973a6fd0bae6
Amr Samir Hamza <ahamza@iskraemeco.com>
2019-06-22 16:24:51 +0200

    Data Size Prog

---
+++ Data size Prog/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Size of int = %d byte\n",sizeof(int));
+    printf("Size of long = %d byte\n",sizeof(long));
+    printf("Size of long long = %d byte\n",sizeof(long long));
+    printf("Size of short = %d byte\n",sizeof(short));
+    printf("Size of float = %d byte\n",sizeof(float));
+    printf("Size of char =%d byte\n",sizeof(char));
+    printf("Size of int = %d byte\n",sizeof(double));
+
+    return 0;
+}
