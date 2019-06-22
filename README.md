ae82f4967b565beb8001b92c4861a619788bad7e
ibrahimshaaban <ibrahim.sh.mohammad@gmail.com>
2019-06-22 16:23:29 +0200

    first app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # M10
+ibrahim shaaban
+test
---
+++ lab1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("size of int = %d\n",sizeof (int));
+    printf("size of char = %d\n",sizeof (char));
+    printf("size of float = %d\n",sizeof (float));
+    printf("size of long = %d\n",sizeof (long));
+    printf("size of long long = %d\n",sizeof (long long));
+    printf("size of short = %d\n",sizeof (short));
+    printf("size of double = %d\n",sizeof (double));
+
+    return 0 ;
+}
