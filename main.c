f433926512c7e90c77d32f9f34ca5989f68e3158
loayboraie <loay.boraie@gmail.com>
2019-06-22 16:18:48 +0200

    loayboraie

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("sizeof int = %d\n",sizeof (int) );
+    printf("sizeof int = %d\n",sizeof (char) );
+    printf("sizeof int = %d\n",sizeof (float) );
+    printf("sizeof int = %d\n",sizeof (long) );
+    printf("sizeof int = %d\n",sizeof (long long) );
+    printf("sizeof int = %d\n",sizeof (short) );
+    printf("sizeof int = %d\n",sizeof (double) );
+    return 0;
+}
