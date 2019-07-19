82d2d95eb760681de8381a7702e1160ff94066b7
AhmedAlaa321 <ahmed.alaa3lloka@gmail.com>
2019-07-19 13:13:21 -0700

    6th session optimization updated

---
+++ CourseWork/6thSessionWork/optimizationinconst/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    const int x = 1;
+    if (x == 1)
+    {
+        printf("Error 1 ");
+    }
+    else
+    {
+        printf("ERROR 2");
+    }
+    return 0;
+}
